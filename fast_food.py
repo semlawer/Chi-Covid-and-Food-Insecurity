8892327bd3cc78a9d33a1d757e540fa410a72911
Valeria Balza Pineda <vbalza@cs.uchicago.edu>
2021-03-16 12:59:50 -0500

    Moved files

---
+++ fast_food.py
@@ -0,0 +1,40 @@
+
+import bs4
+import requests
+#pip3 install fuzzywuzzy[speedup]
+
+
+start = "https://en.wikipedia.org/wiki/List_of_fast_food_restaurant_chains"
+
+
+def read_in(website):
+    '''
+    This function reads in the website if it's a website of interest
+    Input: website
+    Output: Beautiful Soup object of website
+    '''
+    request = requests.get(website)
+    #text = request.text.encode('iso-8859-1')
+    # request_text = get_request(website)
+    # text = read_request(request_text)
+    soup = bs4.BeautifulSoup(request.content, "html.parser")
+    return soup
+
+def scrape(soup):
+    fast_food = []
+    finder_block = soup.find("h5")
+    list_food = finder_block.previousSibling.previousSibling
+    for li in list_food.find_all("li"):
+        fast_food.append(str.upper(li.text))
+    
+    return fast_food
+
+def ff_by_zip(df):
+    '''
+    Collapses data by zip code and returns
+    '''
+    df["ZIP CODE"] = df["ZIP CODE"].astype("string")
+    df[["zip_code", "zip_extra"]] = df["zip"].str.split(".", expand=True)
+    collapse = df["zip_code"].value_counts()
+    collapse.name = "fast_food"
+    return collapse
---
+++ regress.py
@@ -0,0 +1,57 @@
+
+
+
+import pandas as pd
+import numpy as np
+import csv
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.impute import SimpleImputer
+
+def read_in(datas):
+    intro = "{}.csv".format(datas)
+    df = pd.read_csv(intro)
+    return df
+
+def clean_food(food_swamp):
+    food_swamp = food_swamp[food_swamp["zip"] != "NAN"]
+    food_swamp["food_per"] = (food_swamp["Chains"] + food_swamp["Fast Food"]+1)/(
+        food_swamp["grocery_store"]+1)
+    food = food_swamp.rename(columns={"zip":"zipcode"})
+    food = food.drop(["Unnamed: 0"], axis = 1)
+    food = food.astype({"zipcode":int})
+    return food
+
+
+def regression(food, acs, covid):
+    merge = acs.merge(food, on="zipcode", how="inner")
+    merge["fs_ratio"] = (merge["food_per"]/merge["total_population"])*100000
+    X = merge[["perc_black", "perc_hispanic" , "perc_unemployed", "perc_poverty",
+        "perc_homeowners", "median_income"]]
+    y = merge["fs_ratio"]
+    reg = LinearRegression().fit(X, y)
+    predict_food = reg.predict(X)
+    print(merge.columns)
+    score = reg.score(X,y)
+    merge["pr_fs_ratio"] = predict_food
+    merge_full = merge.merge(food, on =["zipcode", "Chains", "Fast Food", "grocery_store", "fs_ratio"], how="outer")
+    merge_full = merge_full.merge(covid, on="zipcode", how="outer").round(2)
+    return merge_full
+
+
+def model(food_swamp_data, acs_data, covid_data):
+    food_swamp = read_in(food_swamp_data)
+    acs = read_in(acs_data)
+    covid = read_in(covid_data)
+    covid = covid.rename(columns={"zip_code":"zipcode"})
+    food = clean_food(food_swamp)
+    regress_results = regression(food, acs, covid)
+    regress_results = regress_results.rename(columns={"perc_non_white":"perc_minority"})
+
+    map_data = regress_results[['zipcode', 'fs_ratio', 'pr_fs_ratio', 
+            'death_rate_cumulative']]
+    table_data = regress_results[['zipcode', 'total_population', 'perc_black',
+            'perc_minority','perc_hispanic','perc_unemployed', 'median_income',
+            'perc_poverty','fs_ratio', 'pr_fs_ratio', 'death_rate_cumulative']]
+    regress_results.to_csv("data/full_zip.csv")
+    return table_data, map_data
