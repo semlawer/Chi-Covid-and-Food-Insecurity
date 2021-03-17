d7b99674bed13d7bd106cde4e819e19adf105a24
Gabriela Palacios Gomez <gabrielapg@uchicago.edu>
2021-03-17 13:29:23 -0500

    db version

--- CS_covid_food/CS_covid_food/settings.py
+++ CS_covid_food/CS_covid_food/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = 'CS_covid_food.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'db_chifood_proj_2',
-        'USER': 'cs_project_2',
+        'NAME': 'db_chifood_proj_3',
+        'USER': 'cs_project_3',
         'HOST': 'localhost',
         'PORT': '5432'
     }
